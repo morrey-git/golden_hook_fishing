938237d6fa9fe5dafb28205050fb190586ad3727
morrey <mengxiangrui@guanmai.cn>
2018-09-25 21:59:55 +0800

    jgdy

---
+++ golden_hook_fishing/core/card.py
@@ -0,0 +1,15 @@
+
+
+class Card(object):
+    def __init__(self):
+        self.__card_id = None
+        self.__card_img = None
+
+    def get_card_id(self):
+        return self.__card_id
+
+
+class CardBox(object):
+    def __init__(self):
+        pass
+
---
+++ golden_hook_fishing/core/core.py
@@ -0,0 +1,13 @@
+
+
+class Core(object):
+    def __init__(self):
+        pass
+
+    def dispatch(self):
+        pass
+
+    def __call__(self):
+        pass
+
+
